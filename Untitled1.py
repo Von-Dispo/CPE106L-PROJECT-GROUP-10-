241e00f5380de07ce9ec1e06cad6dad99aba5d9f
Von-Dispo <161103880+Von-Dispo@users.noreply.github.com>
2024-04-20 15:03:09 +0800

    Bnb_Inventory_Login_partial.py
    Inventory class & login -Dispo

--- Untitled1.py
+++ Untitled1.py
@@ -1,8 +1,49 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Inventory using SQL
+#For the login function, there should be a list of names and list of passwords
 
-# In[ ]:
+listOfNames = ['admin','admin2']
+listOfPasswords = ['pw','pw2']
 
 
 
+class Inventory():   
+
+    def __init__(self, name, password):
+
+        self.name = name #List of all the usernames 
+
+        self.password = password #this should get the password associated with the username in sql 
+
+
+
+    def userLogin(n, pw):
+
+        for i in range(len(listOfNames)):
+
+            if(listOfNames[i] == n and listOfPasswords[i] == pw):
+
+                return True
+
+        else: return False
+
+
+    def currentPatients():
+
+        return
+
+    def addPatient(patient):
+
+        return   
+
+    def addMedicine(medicine):
+
+        return  
+
+    def currentMembers():
+
+        return
+
+    def currentMedicines():
+
+        return
 
